3df532ed67ccd5920865be75a673a00f4830eeda
MrGucheYT <v.smyshlyaev1410@yandex.ru>
2025-06-22 09:41:57 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -15,4 +15,14 @@ def send_sortirovka(message):
 def send_sortirovka(message):
     bot.reply_to(message, "Вот ссылка на самые новые новость в мире экологии: https://ecosphere.press/news/?ysclid=mbxe4llnq9420301768")
 
+@bot.message_handler(commands=['help'])  
+def handle_help(message):  
+    help_text = (  
+        "/start - Начать работу с ботом\n"  
+        "/help - Получить список команд\n"  
+        "/sortirovka - Рассказывает о сортировке мусора\n"
+        "/novosti - Даёт ссылку на все свежие новости об экологии\n"
+    )    
+    bot.send_message(message.chat.id, help_text)  
+
 bot.polling()
